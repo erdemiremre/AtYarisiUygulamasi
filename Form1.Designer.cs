48066bdc34c402c8b2e84c94c36621ce660b89f7
erdemiremre <73900322+erdemiremre@users.noreply.github.com>
2020-11-27 14:19:24 +0200

    güncellendi.

---
+++ Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace AtYarisiUygulaması
+{
+    partial class FormAtYarisi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormAtYarisi));
+            this.atRuzgar = new System.Windows.Forms.PictureBox();
+            this.atSahbatur = new System.Windows.Forms.PictureBox();
+            this.atVeli = new System.Windows.Forms.PictureBox();
+            this.atPoyraz = new System.Windows.Forms.PictureBox();
+            this.atKamil = new System.Windows.Forms.PictureBox();
+            this.btnBasla = new System.Windows.Forms.Button();
+            this.lblFınısh = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblSpiker = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnTekrar = new System.Windows.Forms.Button();
+            this.lblSonDuzluk = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.atRuzgar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.atSahbatur)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.atVeli)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.atPoyraz)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.atKamil)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // atRuzgar
+            // 
+            this.atRuzgar.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.atRuzgar.Image = ((System.Drawing.Image)(resources.GetObject("atRuzgar.Image")));
+            this.atRuzgar.Location = new System.Drawing.Point(12, 106);
+            this.atRuzgar.Name = "atRuzgar";
+            this.atRuzgar.Size = new System.Drawing.Size(142, 122);
+            this.atRuzgar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.atRuzgar.TabIndex = 0;
+            this.atRuzgar.TabStop = false;
+            // 
+            // atSahbatur
+            // 
+            this.atSahbatur.Image = ((System.Drawing.Image)(resources.GetObject("atSahbatur.Image")));
+            this.atSahbatur.Location = new System.Drawing.Point(12, 308);
+            this.atSahbatur.Name = "atSahbatur";
+            this.atSahbatur.Size = new System.Drawing.Size(142, 122);
+            this.atSahbatur.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.atSahbatur.TabIndex = 0;
+            this.atSahbatur.TabStop = false;
+            // 
+            // atVeli
+            // 
+            this.atVeli.Image = ((System.Drawing.Image)(resources.GetObject("atVeli.Image")));
+            this.atVeli.Location = new System.Drawing.Point(12, 409);
+            this.atVeli.Name = "atVeli";
+            this.atVeli.Size = new System.Drawing.Size(142, 122);
+            this.atVeli.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.atVeli.TabIndex = 0;
+            this.atVeli.TabStop = false;
+            // 
+            // atPoyraz
+            // 
+            this.atPoyraz.Image = ((System.Drawing.Image)(resources.GetObject("atPoyraz.Image")));
+            this.atPoyraz.Location = new System.Drawing.Point(12, 207);
+            this.atPoyraz.Name = "atPoyraz";
+            this.atPoyraz.Size = new System.Drawing.Size(142, 122);
+            this.atPoyraz.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.atPoyraz.TabIndex = 0;
+            this.atPoyraz.TabStop = false;
+            // 
+            // atKamil
+            // 
+            this.atKamil.Image = ((System.Drawing.Image)(resources.GetObject("atKamil.Image")));
+            this.atKamil.Location = new System.Drawing.Point(12, 510);
+            this.atKamil.Name = "atKamil";
+            this.atKamil.Size = new System.Drawing.Size(142, 122);
+            this.atKamil.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.atKamil.TabIndex = 0;
+            this.atKamil.TabStop = false;
+            // 
+            // btnBasla
+            // 
+            this.btnBasla.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnBasla.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnBasla.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnBasla.Location = new System.Drawing.Point(12, 25);
+            this.btnBasla.Name = "btnBasla";
+            this.btnBasla.Size = new System.Drawing.Size(130, 36);
+            this.btnBasla.TabIndex = 1;
+            this.btnBasla.Text = "Başla";
+            this.btnBasla.UseVisualStyleBackColor = true;
+            this.btnBasla.Click += new System.EventHandler(this.btnBasla_Click);
+            // 
+            // lblFınısh
+            // 
+            this.lblFınısh.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblFınısh.BackColor = System.Drawing.Color.Yellow;
+            this.lblFınısh.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblFınısh.Location = new System.Drawing.Point(764, 9);
+            this.lblFınısh.Name = "lblFınısh";
+            this.lblFınısh.Size = new System.Drawing.Size(24, 629);
+            this.lblFınısh.TabIndex = 2;
+            this.lblFınısh.Text = "FINISH";
+            this.lblFınısh.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 35;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblSpiker
+            // 
+            this.lblSpiker.BackColor = System.Drawing.Color.Lime;
+            this.lblSpiker.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSpiker.Location = new System.Drawing.Point(200, 25);
+            this.lblSpiker.Name = "lblSpiker";
+            this.lblSpiker.Size = new System.Drawing.Size(461, 75);
+            this.lblSpiker.TabIndex = 3;
+            this.lblSpiker.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(415, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(10, 510);
+            this.label1.TabIndex = 4;
+            // 
+            // btnTekrar
+            // 
+            this.btnTekrar.BackColor = System.Drawing.Color.Red;
+            this.btnTekrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnTekrar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnTekrar.Location = new System.Drawing.Point(12, 67);
+            this.btnTekrar.Name = "btnTekrar";
+            this.btnTekrar.Size = new System.Drawing.Size(130, 33);
+            this.btnTekrar.TabIndex = 5;
+            this.btnTekrar.Text = "Durdur";
+            this.btnTekrar.UseVisualStyleBackColor = false;
+            this.btnTekrar.Click += new System.EventHandler(this.btnTekrar_Click);
+            // 
+            // lblSonDuzluk
+            // 
+            this.lblSonDuzluk.Location = new System.Drawing.Point(651, 100);
+            this.lblSonDuzluk.Name = "lblSonDuzluk";
+            this.lblSonDuzluk.Size = new System.Drawing.Size(10, 510);
+            this.lblSonDuzluk.TabIndex = 4;
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 500;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // FormAtYarisi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(800, 644);
+            this.Controls.Add(this.btnTekrar);
+            this.Controls.Add(this.lblSonDuzluk);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblSpiker);
+            this.Controls.Add(this.lblFınısh);
+            this.Controls.Add(this.btnBasla);
+            this.Controls.Add(this.atKamil);
+            this.Controls.Add(this.atVeli);
+            this.Controls.Add(this.atPoyraz);
+            this.Controls.Add(this.atSahbatur);
+            this.Controls.Add(this.atRuzgar);
+            this.Name = "FormAtYarisi";
+            this.Text = "At Yarışı Oyunu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.atRuzgar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.atSahbatur)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.atVeli)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.atPoyraz)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.atKamil)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox atRuzgar;
+        private System.Windows.Forms.PictureBox atSahbatur;
+        private System.Windows.Forms.PictureBox atVeli;
+        private System.Windows.Forms.PictureBox atPoyraz;
+        private System.Windows.Forms.PictureBox atKamil;
+        private System.Windows.Forms.Button btnBasla;
+        private System.Windows.Forms.Label lblFınısh;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblSpiker;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnTekrar;
+        private System.Windows.Forms.Label lblSonDuzluk;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AtYarisiUygulaması
+{
+    public partial class FormAtYarisi : Form
+    {
+        public FormAtYarisi()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBasla_Click(object sender, EventArgs e)
+        {
+            timer2.Enabled = true;
+            lblSpiker.Text = "Yarış Başlıyor Sayın Seyirciler...";
+            AtlarınDurumDegistir(true);
+            timer1.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            AtlarınDurumDegistir(false);
+
+        }
+        void AtlarınDurumDegistir(bool durum,PictureBox at)
+        {
+            atKamil.Enabled = atPoyraz.Enabled = atRuzgar.Enabled = atSahbatur.Enabled = atVeli.Enabled = durum;
+            at.Enabled = true;
+        }
+        void AtlarınDurumDegistir(bool durum)
+        {
+            atKamil.Enabled = atPoyraz.Enabled = atRuzgar.Enabled = atSahbatur.Enabled = atVeli.Enabled = durum;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            atRuzgar.Left += rnd.Next(1, 8);
+            atVeli.Left+= rnd.Next(1, 8);
+            atSahbatur.Left+= rnd.Next(1, 8);
+            atKamil.Left+= rnd.Next(1, 8);
+            atPoyraz.Left+= rnd.Next(1, 8);
+            KimOnde(atRuzgar);
+            KimOnde(atVeli);
+            KimOnde(atSahbatur);
+            KimOnde(atPoyraz);
+            KimOnde(atKamil);
+            SonDzlkKimOnde(atRuzgar);
+            SonDzlkKimOnde(atVeli);
+            SonDzlkKimOnde(atSahbatur);
+            SonDzlkKimOnde(atPoyraz);
+            SonDzlkKimOnde(atKamil);
+            KimKazandı(atRuzgar);
+            KimKazandı(atVeli);
+            KimKazandı(atSahbatur);
+            KimKazandı(atKamil);
+            KimKazandı(atPoyraz);
+        }
+        void KimKazandı(PictureBox kazanan)
+        {
+            if (kazanan.Right - 15 >= lblFınısh.Left)
+            {
+                timer1.Stop();
+                AtlarınDurumDegistir(false, kazanan);
+                lblSpiker.Text = "Kazanan Atımız:"+" "+kazanan.Name;
+            }
+            
+        }
+        void KimOnde(PictureBox kimonde)
+        {
+            if (kimonde.Right - 10 >= label1.Left)
+            {
+                lblSpiker.Text = " İlk kulvara Önde Gelen Atımız:" + " " + kimonde.Name;
+            }
+        }
+        void SonDzlkKimOnde(PictureBox sndzlk)
+        {
+            if (sndzlk.Right - 10 >= lblSonDuzluk.Left)
+            {
+                lblSpiker.Text = " Son kulvara Önde Gelen Atımız:" + " " + sndzlk.Name;
+            }
+        }
+
+        private void btnTekrar_Click(object sender, EventArgs e)
+        {
+            AtlarınDurumDegistir(false);
+            timer1.Stop();
+            timer2.Enabled = false;
+            lblSpiker.Text = "";
+            atRuzgar.Left = 3;
+            atVeli.Left = 3;
+            atSahbatur.Left = 3;
+            atKamil.Left = 3;
+            atPoyraz.Left = 3;
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (lblSpiker.ForeColor==Color.White)
+            {
+                lblSpiker.ForeColor = Color.Black;
+            }
+            else
+            {
+                lblSpiker.ForeColor = Color.White;
+            }
+        }
+    }
+}
